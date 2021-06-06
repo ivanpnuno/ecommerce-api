88db2a07fb4690adf5f70925dc6ba7469c32fb62
unknown <ivanpnuno@gmail.com>
2021-06-06 21:38:03 +0200

    Swagger added

--- src/main/java/com/ecommerce/Application.java
+++ src/main/java/com/ecommerce/Application.java
@@ -3,7 +3,11 @@ package com.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Ecommerce API", version = "1.0", description = "Prices Information"))
 public class Application {
 
 	public static void main(String[] args) {
